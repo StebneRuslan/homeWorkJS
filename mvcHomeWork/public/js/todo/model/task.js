3bfa73f6f0126072cbd47a1110bc124f3a46987e
Bilmakovchik <ruslanbilamakovskiy@gmail.com>
2017-12-24 15:18:45 +0200

    add files

---
+++ mvcHomeWork/public/js/todo/model/task.js
@@ -0,0 +1,17 @@
+let tasks = new TaskModel([
+    {
+        text: 'Brew coffee',
+        done: true,
+        date: new Date(1999, 11, 17).toLocaleString('ru', options)
+    },
+    {
+        text: 'Write some code',
+        done: false,
+        date: new Date(2000, 11, 17).toLocaleString('ru', options)
+    },
+    {
+        text: 'Sleep',
+        done: false,
+        date: new Date(2018, 11, 17).toLocaleString('ru', options)
+    }
+]);
